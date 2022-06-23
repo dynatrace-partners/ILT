958109c1957db6edd7180116c635551ce1316421
rodrigopascoal <rodrigo.pascoal@gmail.com>
2022-06-23 16:50:43 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,7 @@
 Write-Host "Downloading agent..."
-Invoke-WebRequest -Uri https://ffc96792.live.dynatrace.com/api/v1/deployment/installer/agent/windows/default/latest?Api-Token=dt0c01.I7RMWJW42JZ5NUC77FHQ66KG.XPXC4TOS35EL7DGCNSC3ETDATBKAFCL4Y6HVPUQB2EMZHLPQEXCYCCOO4UUG65GM -$
+start-transcript
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://ffc96792.live.dynatrace.com/api/v1/deployment/installer/agent/windows/default/latest?Api-Token=dt0c01.I7RMWJW42JZ5NUC77FHQ66KG.XPXC4TOS35EL7DGCNSC3ETDATBKAFCL4Y6HVPUQB2EMZHLPQEXCYCCOO4UUG65GM -OutFile "C:\Dynatrace-OneAgent-Windows.exe"
 Write-Host "Unpacking agent..."
 C:\Dynatrace-OneAgent-Windows.exe --quiet
